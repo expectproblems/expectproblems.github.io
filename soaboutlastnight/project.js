ce4beec0facf56582274a6f616ec542fb7854999
expectproblems <elissa_harris@icloud.com>
2022-06-21 15:10:11 +1000

    css tweak

--- soaboutlastnight/project.js
+++ soaboutlastnight/project.js
@@ -1,5 +1,28 @@
 // -----------------------------------
 
+// function to set a given theme/color-scheme
+function setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+}
+// function to toggle between light and dark theme
+function toggleTheme() {
+   if (localStorage.getItem('theme') === 'theme-dark'){
+       setTheme('theme-light');
+   } else {
+       setTheme('theme-dark');
+   }
+}
+// Immediately invoked function to set the theme on initial load
+(function () {
+   if (localStorage.getItem('theme') === 'theme-dark') {
+       setTheme('theme-dark');
+   } else {
+       setTheme('theme-light');
+   }
+})();
+
+
 // always attempt to break to a new line in a way that
 // preserves a minimum number of words per line
 import "./patches/minwordsperline.js";
